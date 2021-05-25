808c581ce32488e9fa595e5affa40b3e4a6d2b07
Mateus Santos <mateusap1@gmail.com>
2021-05-25 18:19:29 -0300

    Fixed authentication page

--- src/pages/authentication/authentication.js
+++ src/pages/authentication/authentication.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import styles from './authentication.module.scss';
 
 export default function Authentication() {
   const addressRef = React.createRef();
+  const history = useHistory();
 
   const updateInput = () => {
     addressRef.current.style.height = "1px";
@@ -12,7 +14,10 @@ export default function Authentication() {
   }
 
   const submitAddress = () => {
-    localStorage.setItem('address', addressRef.current.value);
+    if (addressRef.current.value !== '') {
+      localStorage.setItem('address', addressRef.current.value);
+      history.push('/transactions');
+    }
   }
 
   return (
@@ -20,15 +25,15 @@ export default function Authentication() {
       <div className={styles.mainContent}>
         <div>
           <label htmlFor="address"><b>Nano address</b></label>
-          <textarea
-            name="address" 
-            ref={addressRef}
-            rows={1}
-            onKeyUp={updateInput}
-          />
-          <button>
-            <Link onClick={submitAddress} to="/transactions">Next</Link>
-          </button>
+          <div>
+            <textarea
+              name="address" 
+              ref={addressRef}
+              rows={1}
+              onKeyUp={updateInput}
+            />
+            <button onClick={submitAddress}>Next</button>
+          </div>
         </div>
       </div>
     </>
